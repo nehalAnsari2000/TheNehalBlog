b62a28b6b235c51b1e83c3f829eb0bd4c489071f
Nehal Ansari <66661776+nehalAnsari2000@users.noreply.github.com>
2021-07-04 12:51:14 +0530

    Add files via upload

---
+++ nehal-blog/src/Create.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+
+const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Nehal');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory(); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = {title, body, author};
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(()=>{
+      console.log("New Blog Added");
+      setIsPending(false);
+      history.push('/');     // To redirect the page to home page after adding blog
+    })
+  }
+
+  return ( 
+    <div className='create'>
+      <h2>Add New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title: </label>
+        <input 
+          type="text" 
+          required
+          value = {title}
+          onChange = {(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body: </label>
+        <textarea
+          required
+          value = {body}
+          onChange = {(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog author: </label>
+        <select
+          value = {author}
+          onChange = {(e)=>setAuthor(e.target.value)}
+        >
+          <option value="Nehal">Nehal</option>
+          <option value="Rehan">Rehan</option>
+        </select>
+
+        {!isPending && <button>Add Blog</button>}
+        { isPending && <button disabled>Adding Blog...</button>}
+
+
+      </form>
+    </div>
+   );
+}
+ 
+export default Create;
